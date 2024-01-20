e757815048fb192f86111870582e92e64dc2fdfd
Zoltan Arvai <zoltan.arvai@live.com>
2024-01-20 11:50:50 +0100

    fixed middleware

--- getmytix-web/src/middleware.ts
+++ getmytix-web/src/middleware.ts
@@ -1,6 +1,19 @@
 // middleware.ts
 import { NextRequest, NextResponse } from "next/server";
 
+export const config = {
+  matcher: [
+    /*
+     * Match all paths except for:
+     * 1. /api routes
+     * 2. /_next (Next.js internals)
+     * 3. /_static (inside /public)
+     * 4. all root files inside /public (e.g. /favicon.ico)
+     */
+    "/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)",
+  ],
+};
+
 export function middleware(request: NextRequest) {
   const host = request.headers.get("host");
 
