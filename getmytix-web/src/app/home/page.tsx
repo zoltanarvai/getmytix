e6a5e2a5b5c5b51ebb3728f3526dfff9fc164c40
Zoltan Arvai <zoltan.arvai@live.com>
2024-01-28 14:03:56 +0100

    home page responsive fix

--- getmytix-web/src/app/home/page.tsx
+++ getmytix-web/src/app/home/page.tsx
@@ -1,11 +1,11 @@
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex min-h-screen flex-col items-center pt-24 px-10">
       <section className="flex self-center flex-col mt-20 items-center">
-        <h1 className="text-6xl font-bold tracking-tight text-center">
+        <h1 className="md:text-6xl text-4xl font-bold tracking-tight text-center">
           Üdvözlünk a getmytix.io-n!
         </h1>
-        <h2 className="text-2xl text-gray-500 mt-4 text-center">
+        <h2 className="md:text-2xl text- text-gray-500 mt-4 text-center">
           Jelenleg az oldal karbantartás alatt áll. <br /> Rövidesen a szép, új
           oldalunkkal találkozhattok!
         </h2>
