ae03a46fa05a7290c05ac5766600e316a7ae0385
Zoltan Arvai <zoltan.arvai@live.com>
2024-02-25 00:37:42 +0100

    cleanup

--- getmytix-web/src/app/events/[subdomain]/simplepay/success/page.tsx
+++ getmytix-web/src/app/events/[subdomain]/simplepay/success/page.tsx
@@ -1,5 +1,5 @@
 import {notFound} from "next/navigation";
-import {events, orders, payment} from "@/lib/domain";
+import {events, payment} from "@/lib/domain";
 import {PageTitles} from "@/components/molecules";
 
 type PaymentSuccessfulProps = {
@@ -16,39 +16,20 @@ export default async function PaymentSuccessful({
                                                 }: PaymentSuccessfulProps) {
     const event = await events.getEventBySubdomain(subdomain);
     const paymentResult = payment.getPaymentResponse(r, s);
-    const order = await orders.getOrder(paymentResult.orderId);
 
     if (!event) {
         return notFound();
     }
-
-    if (!order) {
-        throw new Error("Order not found");
-    }
-
-    await orders.fulfill(
-        paymentResult.orderId,
-        paymentResult.transactionId,
-        "simplepay"
-    );
 
     return (
         <main className="flex min-h-screen flex-col max-w-screen-lg m-auto gap-2">
             <PageTitles title={event.name} subtitle="Sikeres jegyvásárlás"/>
 
             <section className="flex flex-col items-center justify-center mt-6 text-center">
-        <span className="font-bold">
-          A jegyeket a megadott email címre küldjük el.
-        </span>
-                <span className="mt-2">
-          Ellenőrizze a Spam és a Levélszemét mappákat is. Előfordulhat, hogy a jegyeket tartalmazó e-mail oda érkezik.
-        </span>
-                <span className="mt-2">
-          (Megrendelési azonosító: {paymentResult.orderId})
-        </span>
-                <span className="mb-4">
-          (Tranzakciós azonosító: {paymentResult.transactionId})
-        </span>
+                <span className="font-bold">A jegyeket a megadott email címre küldjük el.</span>
+                <span className="mt-2">Ellenőrizze a Spam és a Levélszemét mappákat is. Előfordulhat, hogy a jegyeket tartalmazó e-mail oda érkezik.</span>
+                <span className="mt-2">(Megrendelési azonosító: {paymentResult.orderId})</span>
+                <span className="mb-4">(Tranzakciós azonosító: {paymentResult.transactionId})</span>
             </section>
         </main>
     );
--- getmytix-web/src/components/molecules/PageTitles/PageTitles.tsx
+++ getmytix-web/src/components/molecules/PageTitles/PageTitles.tsx
@@ -3,7 +3,7 @@ type PageTitlesProps = {
     subtitle: string;
 };
 
-export const PageTitles: React.FC<PageTitlesProps> = ({title, subtitle}) => {
+export function PageTitles({title, subtitle}: PageTitlesProps) {
     return (
         <section className="flex self-center flex-col mt-20 mb-20 max-md:mb-10 items-center">
             <h1 className="text-6xl max-md:text-5xl font-bold tracking-tight text-center">
