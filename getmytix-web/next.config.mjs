89a8430c828fcf491476324e9f9cefb894593eeb
Zoltan Arvai <zoltan.arvai@live.com>
2024-02-29 14:28:46 +0100

    fix: allowsOrigins

--- getmytix-web/next.config.mjs
+++ getmytix-web/next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
     images: {
         domains: ["networkingkonferencia.hu", "localhost", "framerusercontent.com", "egyertekesites.figyelo.hu"],
     },
+    experimental: {
+        serverActions: {
+            allowedOrigins: ["jegyertekesites.figyelo.hu", "getmytix.io"]
+        }
+    }
 };
 
 export default nextConfig;
