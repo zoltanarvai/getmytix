0102897d3032f5ced0ae6600c137cb2c3ab96621
Zoltan Arvai <zoltan.arvai@live.com>
2024-03-07 08:38:58 +0100

    fix: use non-sandbox payment

--- getmytix-web/src/lib/domain/payment/service/create-payment.ts
+++ getmytix-web/src/lib/domain/payment/service/create-payment.ts
@@ -1,96 +1,97 @@
-import { z } from "zod";
-import {
-  calculateTimeout,
-  generateRandomSalt,
-  generateSignature,
-} from "./utils";
+import {z} from "zod";
+import {calculateTimeout, generateRandomSalt, generateSignature,} from "./utils";
 
 const merchantKey: string = process.env.MERCHANT_KEY!;
 const merchant: string = process.env.MERCHANT_ID!;
+const useSandbox: boolean = process.env.SANDBOX === "YES";
 
 const paymentResponseSchema = z.object({
-  salt: z.string(),
-  merchant: z.string(),
-  orderRef: z.string(),
-  currency: z.string(),
-  transactionId: z.number(),
-  timeout: z.string(),
-  total: z.number(),
-  paymentUrl: z.string(),
+    salt: z.string(),
+    merchant: z.string(),
+    orderRef: z.string(),
+    currency: z.string(),
+    transactionId: z.number(),
+    timeout: z.string(),
+    total: z.number(),
+    paymentUrl: z.string(),
 });
 
 export type PaymentStartResponse = z.infer<typeof paymentResponseSchema>;
 
 if (!merchantKey) {
-  throw new Error("Merchant key not found");
+    throw new Error("Merchant key not found");
 }
 
 async function sendRequest(message: any, url: string) {
-  const payload = JSON.stringify(message);
-  const signature = generateSignature(payload);
+    const payload = JSON.stringify(message);
+    const signature = generateSignature(payload);
 
-  const headers = {
-    "Content-Type": "application/json",
-    Signature: signature,
-  };
+    const headers = {
+        "Content-Type": "application/json",
+        Signature: signature,
+    };
 
-  const response = await fetch(url, {
-    method: "POST",
-    headers: headers,
-    body: payload,
-  });
+    const response = await fetch(url, {
+        method: "POST",
+        headers: headers,
+        body: payload,
+    });
 
-  return response;
+    return response;
 }
 
 export async function createPayment(
-  orderId: string,
-  customerEmail: string,
-  amount: string,
-  redirectUrl: string,
-  invoiceDetails: {
-    name: string;
-    company: string;
-    country: string;
-    state: string;
-    city: string;
-    zip: string;
-    address: string;
-  }
+    orderId: string,
+    customerEmail: string,
+    amount: string,
+    redirectUrl: string,
+    invoiceDetails: {
+        name: string;
+        company: string;
+        country: string;
+        state: string;
+        city: string;
+        zip: string;
+        address: string;
+    }
 ): Promise<PaymentStartResponse> {
-  const message = {
-    salt: generateRandomSalt(16),
-    merchant: merchant,
-    orderRef: orderId,
-    currency: "HUF",
-    customerEmail,
-    language: "HU",
-    sdkVersion:
-      "SimplePayV2.1_Payment_PHP_SDK_2.0.7_190701:dd236896400d7463677a82a47f53e36e",
-    methods: ["CARD"],
-    total: amount,
-    timeout: calculateTimeout(5),
-    urls: {
-      success: `${redirectUrl}/simplepay/success`,
-      fail: `${redirectUrl}/simplepay/fail`,
-      cancel: `${redirectUrl}/simplepay/cancel`,
-      timeout: `${redirectUrl}/simplepay/timeout`,
-    },
-    invoice: invoiceDetails,
-    threeDSReqAuthMethod: "01",
-  };
+    const message = {
+        salt: generateRandomSalt(16),
+        merchant: merchant,
+        orderRef: orderId,
+        currency: "HUF",
+        customerEmail,
+        language: "HU",
+        sdkVersion:
+            "SimplePayV2.1_Payment_PHP_SDK_2.0.7_190701:dd236896400d7463677a82a47f53e36e",
+        methods: ["CARD"],
+        total: amount,
+        timeout: calculateTimeout(5),
+        urls: {
+            success: `${redirectUrl}/simplepay/success`,
+            fail: `${redirectUrl}/simplepay/fail`,
+            cancel: `${redirectUrl}/simplepay/cancel`,
+            timeout: `${redirectUrl}/simplepay/timeout`,
+        },
+        invoice: invoiceDetails,
+        threeDSReqAuthMethod: "01",
+    };
 
-  const response = await sendRequest(
-    message,
-    "https://sandbox.simplepay.hu/payment/v2/start"
-  );
+    const url = useSandbox ?
+        "https://sandbox.simplepay.hu/payment/v2/start" :
+        "https://simplepay.hu/payment/v2/start";
 
-  if (response.ok) {
-    const responseBody = await response.json();
-    console.info("Payment response", responseBody);
+    const response = await sendRequest(
+        message,
+        url
+    );
 
-    return paymentResponseSchema.parse(responseBody);
-  } else {
-    throw new Error("Failed to create payment - " + (await response.text()));
-  }
+    if (response.ok) {
+        const responseBody = await response.json();
+        console.info("Payment response", responseBody);
+
+        return paymentResponseSchema.parse(responseBody);
+    } else {
+        throw new Error("Failed to create payment - " + (await response.text()));
+    }
 }
