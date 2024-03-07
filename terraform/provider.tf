f6292bc11c31c7cf301647cf3cf54d341b2a2012
Zoltan Arvai <zoltan.arvai@live.com>
2024-03-07 12:04:31 +0100

    fix: stuff

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,12 +5,6 @@ terraform {
       version = "5.31.0"
     }
   }
-
-  backend "s3" {
-    bucket  = "getmytix-terraform-state"
-    region  = "eu-central-1"
-    key     = "state/terraform.prod.tfstate"
-  }
 }
 
 provider "aws" {
